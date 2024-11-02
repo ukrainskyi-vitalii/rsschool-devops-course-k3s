dda5220bfa2d272ac30f6fb29e150988bf2f20ab
Vitaliy Ukrainskiy <vitaliy.ukrainskiy@gameloft.com>
2024-11-02 16:46:04 +0200

    feat: add jenkins

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ module "vpc" {
 module "k3s" {
   source = "./modules/k3s"
 
-  ami_id              = var.ami_id
-  vpc_id              = module.vpc.main_vpc_id
-  vpc_cidr            = module.vpc.main_vpc_cidr
-  ssh_key_name        = var.ssh_key_name
-  public_subnet_id    = module.vpc.public_subnet_1_id
+  ami_id           = var.ami_id
+  vpc_id           = module.vpc.main_vpc_id
+  vpc_cidr         = module.vpc.main_vpc_cidr
+  ssh_key_name     = var.ssh_key_name
+  public_subnet_id = module.vpc.public_subnet_1_id
 }
