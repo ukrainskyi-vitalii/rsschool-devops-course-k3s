a52fa05bd62f1bdd7dc4e722ded840e296351936
Vitaliy Ukrainskiy <vitaliy.ukrainskiy@gameloft.com>
2024-11-13 18:22:00 +0200

    feat: workflow refactoring

--- .github/scripts/jenkins_configs.sh
+++ .github/scripts/jenkins_configs.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "HOSTNAME: $HOSTNAME"
+echo "USER_NAME: $USER_NAME"
+
 scp -o StrictHostKeyChecking=no -i private_key helm-configs/jenkins-volume.yaml ${USER_NAME}@${HOSTNAME}:/home/${USER_NAME}/jenkins-volume.yaml
 scp -o StrictHostKeyChecking=no -i private_key helm-configs/jenkins-sa.yaml ${USER_NAME}@${HOSTNAME}:/home/${USER_NAME}/jenkins-sa.yaml
 scp -o StrictHostKeyChecking=no -i private_key helm-configs/jenkins-values.yaml ${USER_NAME}@${HOSTNAME}:/home/${USER_NAME}/jenkins-values.yaml
