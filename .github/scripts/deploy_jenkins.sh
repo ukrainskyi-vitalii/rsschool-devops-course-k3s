efbb8ed9496c5d3c7d5718ee06c3fe2b83bc01f0
Vitaliy Ukrainskiy <vitaliy.ukrainskiy@gameloft.com>
2024-11-13 18:39:24 +0200

    feat: workflow refactoring

--- .github/scripts/deploy_jenkins.sh
+++ .github/scripts/deploy_jenkins.sh
@@ -9,9 +9,9 @@ sudo chmod 777 /data/jenkins-volume &&
 ls -ld /data/jenkins-volume &&
 
 # Apply Jenkins configurations
-sudo KUBECONFIG=/etc/rancher/k3s/k3s.yaml /usr/local/bin/kubectl apply -f /home/${USER_NAME}/jenkins-volume.yaml
-sudo KUBECONFIG=/etc/rancher/k3s/k3s.yaml /usr/local/bin/kubectl apply -f /home/${USER_NAME}/jenkins-sa.yaml
+sudo KUBECONFIG=/etc/rancher/k3s/k3s.yaml /usr/local/bin/kubectl apply -f jenkins-volume.yaml
+sudo KUBECONFIG=/etc/rancher/k3s/k3s.yaml /usr/local/bin/kubectl apply -f jenkins-sa.yaml
 
 # Install Jenkins using Helm
-KUBECONFIG=/etc/rancher/k3s/k3s.yaml helm install jenkins -n jenkins -f /home/${USER_NAME}/jenkins-values.yaml jenkins/jenkins
+KUBECONFIG=/etc/rancher/k3s/k3s.yaml helm install jenkins -n jenkins -f jenkins-values.yaml jenkins/jenkins
 
