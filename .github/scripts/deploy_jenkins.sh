4250eae2ca75ea47235d732522d4cf1f3e96368c
Vitaliy Ukrainskiy <vitaliy.ukrainskiy@gameloft.com>
2024-11-13 18:00:51 +0200

    feat: workflow refactoring

--- .github/scripts/deploy_jenkins.sh
+++ .github/scripts/deploy_jenkins.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Create Jenkins volume directory
-sudo mkdir -p /data/jenkins-volume
-sudo chmod 777 /data/jenkins-volume
+sudo mkdir -p /data/jenkins-volume &&
+sudo chmod 777 /data/jenkins-volume &&
+ls -ld /data/jenkins-volume &&
 
 # Apply Jenkins configurations
-kubectl apply -f /home/${USER_NAME}/jenkins-volume.yaml
-kubectl apply -f /home/${USER_NAME}/jenkins-sa.yaml
+sudo KUBECONFIG=/etc/rancher/k3s/k3s.yaml /usr/local/bin/kubectl apply -f /home/${USER_NAME}/jenkins-volume.yaml
+sudo KUBECONFIG=/etc/rancher/k3s/k3s.yaml /usr/local/bin/kubectl apply -f /home/${USER_NAME}/jenkins-sa.yaml
 
 # Install Jenkins using Helm
-helm install jenkins -n jenkins -f /home/${USER_NAME}/jenkins-values.yaml jenkins/jenkins
+KUBECONFIG=/etc/rancher/k3s/k3s.yaml helm install jenkins -n jenkins -f /home/${USER_NAME}/jenkins-values.yaml jenkins/jenkins
 
--- .github/scripts/initialize_k3s.sh
+++ .github/scripts/initialize_k3s.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # Add Jenkins Helm repository
-helm repo remove jenkins || true
-helm repo add jenkins https://charts.jenkins.io
-helm repo update
+/usr/local/bin/helm repo remove jenkins || true &&
+/usr/local/bin/helm repo add jenkins https://charts.jenkins.io &&
+/usr/local/bin/helm repo update &&
+/usr/local/bin/helm search repo jenkins || exit 1 && 
 
 # Wait for k3s to initialize fully
-until kubectl get nodes; do
+until sudo KUBECONFIG=/etc/rancher/k3s/k3s.yaml /usr/local/bin/kubectl get nodes; do
   echo 'Waiting for k3s server to be ready...'
   sleep 10
-done
+done &&
 
 # Create Jenkins namespace and set context
-kubectl create namespace jenkins || true
-kubectl config set-context --current --namespace=jenkins
-kubectl get namespaces
+sudo KUBECONFIG=/etc/rancher/k3s/k3s.yaml /usr/local/bin/kubectl create namespace jenkins || true &&
+sudo KUBECONFIG=/etc/rancher/k3s/k3s.yaml /usr/local/bin/kubectl config set-context --current --namespace=jenkins &&
+sudo KUBECONFIG=/etc/rancher/k3s/k3s.yaml /usr/local/bin/kubectl get namespaces
