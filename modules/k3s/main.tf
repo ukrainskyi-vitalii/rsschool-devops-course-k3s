c7060caa923abcecaeab68b8919e7cc38114edb1
Vitaliy Ukrainskiy <vitaliy.ukrainskiy@gameloft.com>
2024-11-24 12:46:53 +0200

    feat: increase ec2 instance

--- modules/k3s/main.tf
+++ modules/k3s/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "k3s_master" {
   ami             = var.ami_id
-  instance_type   = "t3.medium"
+  instance_type   = "t2.small"
   subnet_id       = var.public_subnet_id
   key_name        = var.ssh_key_name
   security_groups = [aws_security_group.k3s_sg.id]
