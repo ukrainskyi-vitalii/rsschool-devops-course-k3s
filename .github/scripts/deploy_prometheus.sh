ce35cb51b90673f607f1851ab916d7fd4e24bf74
Vitaliy Ukrainskiy <vitaliy.ukrainskiy@gameloft.com>
2024-11-30 16:26:27 +0200

    feat: add prometheus job

--- .github/scripts/deploy_prometheus.sh
+++ .github/scripts/deploy_prometheus.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+echo "Setting up kubeconfig for k3s..."
+
+cat /etc/rancher/k3s/k3s.yaml
+
+mkdir -p ~/.kube
+
+sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+
+sudo chown $(id -u):$(id -g) ~/.kube/config
+
+echo "kubeconfig setup completed!"
+
 echo "Deploying Prometheus using Helm..."
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
