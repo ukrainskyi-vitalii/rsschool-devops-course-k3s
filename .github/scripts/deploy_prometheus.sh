7fb0b1d4fc204dd4d16906e8d681add22222522d
Vitaliy Ukrainskiy <vitaliy.ukrainskiy@gameloft.com>
2024-11-30 16:44:57 +0200

    feat: add prometheus job

--- .github/scripts/deploy_prometheus.sh
+++ .github/scripts/deploy_prometheus.sh
@@ -18,10 +18,8 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
 helm install prometheus bitnami/prometheus \
-  --namespace jenkins \
-  --create-namespace \
-  --set-file serverFiles.prometheus.yml=/home/ubuntu/prometheus-config.yaml \
-  --set server.service.type=NodePort \
-  --set server.service.nodePorts.http="\"32002\""
+   --namespace jenkins \
+   --set server.service.type=NodePort \
+   --set-string server.service.nodePorts.http=32002
 
 echo "Prometheus deployment completed!"
